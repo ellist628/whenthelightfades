36f806bcfde669762bbede9b8f4f72f051d39d64
ellist628 <tim.ellis62802@gmail.com>
2025-02-13 10:03:24 -0600

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -9,21 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// function filterAndSortEnemies() {
-//   const searchQuery = document.getElementById("searchBar").value.toLowerCase();
-//   const sortValue = document.getElementById("sortFilter").value;
-
-//   let filteredEnemies = enemyData.filter(enemy => enemy.name.toLowerCase().includes(searchQuery));
-
-//   if (sortValue === "a-z") {
-//     filteredEnemies.sort((a, b) => a.name.localeCompare(b.name));
-//   } else if (sortValue === "z-a") {
-//     filteredEnemies.sort((a, b) => b.name.localeCompare(a.name));
-//   }
-
-//   displayEnemies(filteredEnemies);
-// }
-
 function displayEnemies(enemies) {
   const enemyList = document.getElementById("enemy-list");
   enemyList.innerHTML = "";
